11645ec7d3a34633cd642c7c4295362adc0fcb90
Patrik Sokolowski <wunderward@gmail.com>
2024-10-15 07:15:22 -0400

    working state

--- test_main.py
+++ test_main.py
@@ -29,13 +29,13 @@ def test_health(client):
 
 
 def test_auth(client):
-    # body = {'email': EMAIL,
-    #         'password': PASSWORD}
-    # response = client.post('/auth', 
-    #                        data=json.dumps(body),
-    #                        content_type='application/json')
+    body = {'email': EMAIL,
+            'password': PASSWORD}
+    response = client.post('/auth', 
+                           data=json.dumps(body),
+                           content_type='application/json')
 
-    # assert response.status_code == 200
-    # token = response.json['token']
-    # assert token is not None
-    assert False
+    assert response.status_code == 200
+    token = response.json['token']
+    assert token is not None
+    # assert False
